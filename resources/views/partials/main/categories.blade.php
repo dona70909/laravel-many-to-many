fc7d59857d574b31b38418f7787713343a4baab5
Donatella7 <campaniellodonatella@gmail.com>
2022-05-24 17:10:50 +0200

    Delete

--- resources/views/partials/main/categories.blade.php
+++ resources/views/partials/main/categories.blade.php
@@ -8,9 +8,32 @@
                     <a href="{{route('categories.show',$category)}}" class="btn btn-secondary bt-sm text-white fs-6 text-uppercase fw-bolder">Category</a>
 
                     <a href="{{route('categories.edit',$category)}}" class="btn btn-secondary bt-sm text-white fs-6 text-uppercase fw-bolder">Edit category</a>
+
+                    <form action="{{route('admin.categories.destroy', $post)}}" method="POST" class="category-form-destroyer" category-name="{{$category->name}}">
+                        @csrf
+                        @method('DELETE')
+
+                        <button type="submit"  class="btn btn-danger btn-sm ">Delete</a>
+                    </form>
                 </div>
             </div>
         @endforeach
         
     </div>
 </section>
+
+@section('footer-scripts')
+    <script defer>
+        const deleteForms = document.querySelectorAll('.category-form-destroyer');
+        console.log(deleteForms);
+        deleteForms.forEach(singleForm => {
+            singleForm.addEventListener('submit', function (event) {
+                event.preventDefault(); // § blocchiamo l'invio del form
+                userConfirmation = window.confirm(`Are you sure  ${this.getAttribute('category-name')}?` );
+                if (userConfirmation) {
+                    this.submit();
+                }
+            })
+        });
+    </script>
+@endsection
