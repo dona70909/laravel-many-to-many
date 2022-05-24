4866c066efb0dcd62b7118e3d915f8a9c4c8d6af
Donatella7 <campaniellodonatella@gmail.com>
2022-05-24 22:51:51 +0200

    Deleted comment

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -69,7 +69,7 @@ class PostController extends Controller
         $post->save();
 
         $post->categories()->sync($data['category']);
-        
+
         /* redirect to the show of the new Post update */
         return redirect()->route('posts.show',["post" => $post]);
     }
@@ -80,9 +80,8 @@ class PostController extends Controller
      * @param  int  Post post
      * @return \Illuminate\Http\Response
      */
-    public function show(Post $post,Request $request)
+    public function show(Post $post)
     {
-        dump($request);
         return view('admin.posts.show',compact('post'));
     }
     /**
